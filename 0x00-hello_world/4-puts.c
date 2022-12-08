05d0db520523a0f5e07c85359563fad68acbdbef
Abdurrahman <dreydare16@gmail.com>
2022-12-08 04:30:04 -0800

    Updated some syntaxes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-#include <stdlib.h>
-
 /**
+ * main - Entry point for print code
  *
- *  * main - Entry point for print code
- *
- *   * Return: Always 0 (Success)
- *
- *    */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
