60eaa9d0a0479891b5cec3db0dc92451446b3e22
Abdurrahman <dreydare16@gmail.com>
2022-12-09 04:16:18 -0800

    updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,14 +4,23 @@
  * main - Entry point
  *
  * Return: 0
+ *
  */
 
 int main(void)
 {
 
+	int num;
 	char letters;
 
-	for (letters = 'z'; letters >= 'a'; letters--)
+	for (num = '0' ; num <= '9' ; num++)
+	{
+
+		putchar(num);
+
+	}
+
+	for (letters = 'a' ; letters <= 'f' ; letters++)
 	{
 
 		putchar(letters);
