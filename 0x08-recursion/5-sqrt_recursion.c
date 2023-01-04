16fe22bf2a039f0590944a50d55f6440fb200344
Abdurrahman <dreydare16@gmail.com>
2023-01-04 09:26:45 -0800

    updating

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,31 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * _sqrt_recursion - prints the square root of a number
- * @n: String argument
- * Return: square root value
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+
+int _sqrt(int n, int i)
+{
+		if (n < 0)
+			return (-1);
+		if ((i * i) > n)
+			return (-1);
+		if (i * i == n)
+			return (i);
+
+		return (_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - main funct
+ * @n: int n
+ * Return: int
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	else if (n < 0)
-		return (-1);
-	else
-		return (squarecheck(n, 1));
-}
-
-/**
- * squarecheck - check for square value for n
- * @n: number
- * @i: value to increment
- * Return: i
- */
-
-int squarecheck(int n, int i)
-{
-	if (n == (i * i))
-		return (i);
-	else if (n > (i * i))
-		return (squarecheck(n, i + 1));
-	else
-		return (-1);
+		return (_sqrt(n, 1));
 }
